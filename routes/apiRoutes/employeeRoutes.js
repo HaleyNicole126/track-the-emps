dc30c03941308f76a086dd34a4e3318e0a6496d9
haybales <haleynicole126@gmail.com>
2022-05-18 17:20:23 -0600

    update and delete employee

--- routes/apiRoutes/employeeRoutes.js
+++ routes/apiRoutes/employeeRoutes.js
@@ -63,9 +63,9 @@ router.post("/employee", ({ body }, res) => {
     res.status(400).json({ error: errors });
     return;
   }
-  const sql = `INSERT INTO employee (body.first_name, body.last_name, body.role_id, body.manager_id)
-    VALUES (?, ?, ?, ?)`;
-  const params = [body.name];
+  const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+    VALUES (?,?,?,?)`;
+  const params = [body.first_name, body.last_name, body.role_id, body.manager_id];
 
   db.query(sql, params, (err, result) => {
     if (err) {
@@ -80,8 +80,53 @@ router.post("/employee", ({ body }, res) => {
 });
 
 // Update employee's role
+router.put('/employee/:id', (req, res) => {
+    const errors = inputCheck(req.body, 'role_id');
 
+    if(errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+    const params = [req.body.role_id, req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Employee not found'
+            });
+        } else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
+    });
+});
 
 // DELETE an employee
+router.delete('/employee/:id', (req, res) => {
+    const sql = `DELETE FROM employee WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.statusMessage(400).json({ error: res.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
 
 module.exports = router;
